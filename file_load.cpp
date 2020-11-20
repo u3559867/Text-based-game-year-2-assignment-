9d4bdd76534c47443e46e35ffb468ff40eb7acf8
u3559867 <73229869+u3559867@users.noreply.github.com>
2020-11-20 15:06:15 +0800

    Create file_load.cpp

---
+++ file_load.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ifstream fin;
+	fin.open(filename); //file that load in
+
+	//file cannot open
+	if (fin.fail())
+	{
+		cout << "Error in file opening!" << endl;
+		exit(1);
+	}
+
+	//int x[8]; it represents the game status
+	//load in game status from file
+	for (int i = 0; i < 8; i++)
+	{
+		fin >> x[i];
+	}
+
+	fin.close();
+
+	return 0;
+}
