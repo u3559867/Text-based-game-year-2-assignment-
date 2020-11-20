064c4783f4b44b965349d1758e947ee82f5b0a12
u3559867 <73229869+u3559867@users.noreply.github.com>
2020-11-20 15:04:48 +0800

    I did not make this file
    This file is made by Alex.

---
+++ file_save.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+
+
+using namespace std;
+
+int main()
+{
+	ofstream fout;
+	fout.open(filename); //file that stored in
+
+	//file cannot open
+	if (outputfile.fail())
+	{
+		cout << "Error in file opening!" << endl;
+		exit(1);
+	}
+	
+	//int x[8]; it represents the game status
+	//store game status
+	for(int i = 0; i < 8; i++)
+	{
+		fout << x[i];
+	}
+    
+	fout.close();
+
+	return 0;
+}
